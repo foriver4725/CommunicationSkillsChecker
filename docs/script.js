aa381f93564d6697795b0d5caa2c5f6aecf23634
foriver4725 <hrt67@icloud.com>
2026-04-22 13:59:24 +0900

    Update SHARE_URL to point to the repository and use it in the sharing function

--- docs/script.js
+++ docs/script.js
@@ -7,6 +7,9 @@ const DATA_URL = "./diagnosis-data.json";
 // テスト用(質問1件だけ):
 // const DATA_URL = "./diagnosis-data-dummy-single.json";
 
+// X共有用URL
+const SHARE_URL = "https://github.com/foriver4725/CommunicationSkillsChecker/";
+
 /**
  * アプリ全体の状態
  * - data: 読み込んだ診断データ
@@ -267,7 +270,7 @@ function handleShareX() {
     `あなたも試してみてね。\n` +
     `#コミュニケーション診断`;
 
-  const pageUrl = window.location.href;
+  const pageUrl = SHARE_URL;
   const url =
     "https://x.com/intent/post?text=" +
     encodeURIComponent(text) +
